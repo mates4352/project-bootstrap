7f1236f02effd8a87e2c8103d38d14651107e30c
Sergey <cabyrkih@mail.ru>
2021-02-16 22:47:05 +0300

    accordion

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 const play = document.querySelector(".play__button")
 const video = document.querySelector(".modal__video")
 const moda = document.querySelector(".moda")
+const body = document.querySelector('.body')
 
 window.addEventListener('click', (event) => {
 	if (event.target.classList.contains("play__button")) {
 		video.play()
 		moda.classList.add("active")
+		body.classList.add("lock")
 	}
 	if (event.target.classList.contains("moda")) {
 		video.pause()
 		moda.classList.remove("active")
+		body.classList.remove("lock")
 	}
 })
